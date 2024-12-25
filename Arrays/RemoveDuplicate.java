3119c42eb54d9a025a718b36076e29986944b503
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2024-12-25 17:09:03 +0530

    Two Pointer

---
+++ Arrays/RemoveDuplicate.java
@@ -0,0 +1,39 @@
+class RemoveDuplicate {
+
+    public static void main(String[] args){
+        int[] nums = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3};
+        for(int i: nums){
+            System.out.print(i + " ");
+        }
+        System.out.println("Number of unique elements "+ removeDuplicates(nums));
+        for(int i: nums){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int size = nums.length;
+        int uniq = 0;
+        int jump = 0;
+        int counter = 1;
+
+        if(size==1){
+                return 1;
+            }
+
+        while(jump<size){
+            
+            if(nums[uniq]==nums[jump]){
+                jump++;
+            }
+            else if(nums[uniq]!= nums[jump]){
+                uniq++;
+                nums[uniq]=nums[jump];
+                jump++;
+                counter++;
+            }
+        }
+        return counter;
+        
+    }
+}
