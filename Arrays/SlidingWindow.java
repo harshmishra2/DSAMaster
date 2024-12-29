50fbf254ec4fc5e4e11f392edbe7772de514f700
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2024-12-29 23:59:40 +0530

    Update SlidingWindow.java

--- Arrays/SlidingWindow.java
+++ Arrays/SlidingWindow.java
@@ -1,5 +1,12 @@
 class SlidingWindow {
-    public double findMaxAverage(int[] nums, int k) {
+
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7};
+        int k = 5;
+        System.out.println("The maximum avg of consecutive k elements is" + findMaxAverage(nums, k));
+    }
+    
+    public static double findMaxAverage(int[] nums, int k) {
 
         double result = 0.0;
         for(int i= 0; i<k; i++){
