9f0c99f7084c2588f69c5874c1c3f157bb3ee756
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-01-04 01:24:43 +0530

    Greedy TwoPointer Array

---
+++ Arrays/ContainerWMostWater.java
@@ -0,0 +1,34 @@
+class ContainerWMostWater {
+
+    public static void main(String [] args){
+        int [] arr = new int[] {4, 3, 2, 1, 1, 2, 3, 4, 5};
+        System.out.println("The max possible area is - "+ maxArea(arr));
+    }
+
+
+    public static int maxArea(int[] height) {
+        int end = height.length -1;
+        int start = 0;
+        int area = 0;
+
+        while(start<end){
+            if(height[start]>height[end]){
+                int curArea = (end-start)*height[end];
+                end--;
+                if(curArea>area){
+                    area = curArea;
+                }
+            }
+            else{
+                int curArea = (end-start)*height[start];
+                start++;
+                if(curArea>area){
+                    area = curArea;
+                }
+            }
+            
+        }
+        return area;
+    }
+}
+//https://leetcode.com/problems/container-with-most-water/submissions/?envType=study-plan-v2&envId=top-interview-150
