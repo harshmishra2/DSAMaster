857b4ab76b60a78fe32b0b4d4883f595fdf4286a
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-01-06 23:44:16 +0530

    Add files via upload

---
+++ Arrays/JumpGame.java
@@ -0,0 +1,35 @@
+class JumpGame {
+
+    public static void main (String [] args){
+        System.out.println("Can we reach the array's last element in the given arrays?");
+
+        int[] normal = {2, 3, 1, 1, 4};
+        int [] edge0 = {0};
+        int [] edge1 = {1};
+
+        System.out.println(canJump(normal));
+        System.out.println(canJump(edge0));
+        System.out.println(canJump(edge1));
+
+    }
+
+    public static boolean canJump(int[] nums) {
+
+        int maxReach = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            if(i>maxReach){
+                return false;
+            }
+            //maxReach = Math.max(maxReach, i + nums[i]);
+            if(nums[i]+i>maxReach){
+                maxReach = nums[i] + i;
+            }
+            if(maxReach >= nums.length-1){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
