f2fe717a1e003e4f33e25718947609737875ed4c
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-01-07 23:42:45 +0530

    Add files via upload

---
+++ String/IsSubsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean isSubsequence(String s, String t) {
+
+        int sp = 0;
+        int tp = 0;
+
+        while(sp<s.length() & tp<t.length()){
+            if(s.charAt(sp)== t.charAt(tp)){
+                sp++;
+            }
+            tp++;
+        }
+        return sp == s.length();
+    }
+}
