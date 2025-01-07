94fccd019967dd1db78778abfc5be16a3a5197cf
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-01-07 23:45:40 +0530

    TwoPointer String Dynamic

--- String/IsSubsequence.java
+++ String/IsSubsequence.java
@@ -1,4 +1,12 @@
-class Solution {
+class IsSubsequence {
+
+    public static void main(String [] args){
+        String s = "ace";
+        String t = "abcghpde";
+        System.out.println("Does the string contain the given subsequence"+isSubsequence(s, t));
+    
+    }
+    
     public static boolean isSubsequence(String s, String t) {
 
         int sp = 0;
@@ -13,3 +21,5 @@ class Solution {
         return sp == s.length();
     }
 }
+
+//https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
