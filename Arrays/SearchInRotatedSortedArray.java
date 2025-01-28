bd419d09950b91f75491d863de5e79e0dd3a9474
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-01-28 18:54:04 +0530

    Array BinarySearch

---
+++ Arrays/SearchInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>=nums[left]){
+                if(nums[left]<=target && nums[mid]>target){
+                    right = mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }else{
+                if(target<=nums[right] && target>nums[mid]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
