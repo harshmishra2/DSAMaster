62c2f42545b579f9c41381152ae8619774d7c31b
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-02-18 19:05:24 +0530

    Array Sorting CountingSort

---
+++ Arrays/HIndex.java
@@ -0,0 +1,17 @@
+class HIndex {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        int h = 0;
+        int len = citations.length;
+
+        for(int i=0; i<len; i++){
+            if(citations[i]>=len-i){
+                return len-i;
+            }
+        }
+
+        return 0;
+        
+    }
+}
