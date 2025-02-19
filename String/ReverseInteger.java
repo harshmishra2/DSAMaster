ea94da66e4ed02b74be6a5ecec5a38ef56e57fdc
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-02-19 14:57:04 +0530

    String Math

--- String/ReverseInteger.java
+++ String/ReverseInteger.java
@@ -33,3 +33,35 @@ class ReverseInteger {
 
 //self made solution so isn't the most efficient will work on this problem again
 //https://leetcode.com/problems/reverse-integer/description/
+
+
+class ReverseInteger {
+    public int reverse(int x) {
+
+        long finalSum = 0;
+        boolean isNegat = false;
+        if(x<0){
+            isNegat = true;
+        }
+
+        while(x!=0){
+            finalSum *= 10;
+            finalSum += x%10;
+            x /= 10;
+        }
+
+        if(finalSum>Integer.MAX_VALUE || finalSum<Integer.MIN_VALUE){
+            return 0;
+        }
+        else{
+            if(isNegat){
+                return (int)finalSum;
+            }else{
+                return (int)finalSum;
+            }
+        }
+        
+    }
+}
+
+//the second code is much more optimised compared to the first one.
